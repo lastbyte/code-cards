969be049f00f2525e6fb4b12be0197fb4a77db03
nishant <nishant.9c9@gmail.com>
2023-12-01 02:50:21 +0530

    Updates

--- assets/app-store.ffa169b5.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.c8a8414a.js";const a=e("appStore",{state:()=>({drawer:!1,breadcrumb:void 0}),getters:{},actions:{toggleDrawer(){this.drawer=!this.drawer},setBreadcrumb(r){this.breadcrumb=r}}});export{a as u};
